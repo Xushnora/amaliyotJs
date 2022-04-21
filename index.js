2f249edcea7b5661698baa535c498debbfc6a42a
Xushnora <xushnoranishonova@gmail.com>
2022-04-20 20:02:40 -0700

    changed

--- index.js
+++ index.js
@@ -20,34 +20,5 @@
 // }
 // console.log(getSatr(a));
 
-// #214
-
-let arr = [1,2,2,1]
-let arr2 =[1,2,2,2]
-
-let palindromeArray = (arr) => {
-  
-     return isPalindrome(arr);
-    
-}
-function isPalindrome(arr) {
-    let isPalindromeNumber = true
-
-    for(let i = 0; i < arr.length / 2; i++) {
-
-        if(arr[i] !== arr[arr.length - i - 1]){
-            isPalindromeNumber = false; 
-          break;
-        }
-    }
-    
-    return isPalindromeNumber;
-}
-
-console.log(palindromeArray(arr));
-console.log(palindromeArray(arr2));
-
-
-
 
 
